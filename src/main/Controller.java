9fbf0f6715b82d5b1e04965441bfc72470fca2fd
sairakaye <30943505+sairakaye@users.noreply.github.com>
2018-08-17 20:35:36 +0800

    Animation of the rocketship added.

--- src/main/Controller.java
+++ src/main/Controller.java
@@ -1,5 +1,48 @@
 package main;
 
-public class Controller {
+import com.jfoenix.controls.JFXButton;
+import javafx.animation.Animation;
+import javafx.animation.TranslateTransition;
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 
+public class Controller {
+    @FXML ImageView rocket;
+    @FXML JFXButton lezzGo;
+    private boolean isOnEarth = true;
+
+    public void goToAnotherPlace() {
+        TranslateTransition animateRocket = new TranslateTransition(Duration.millis(2000), rocket);
+
+        animateRocket.statusProperty().addListener((observableValue, oldValue, newValue) -> {
+            if (newValue == Animation.Status.RUNNING)
+                System.out.println("Animation is still running.");
+            else {
+                System.out.println("Animation done!");
+
+                if (isOnEarth) {
+                    rocket.setRotate(50f);
+                    // original 50
+                    //
+                } else {
+                    rocket.setRotate(-130f);
+                }
+            }
+
+        });
+
+        if (isOnEarth) {
+            animateRocket.setByX(300f);
+            animateRocket.setByY(-300f);
+            isOnEarth = false;
+        } else {
+            animateRocket.setByX(-300f);
+            animateRocket.setByY(300f);
+            isOnEarth = true;
+        }
+
+        animateRocket.setRate(1.5f);
+        animateRocket.play();
+    }
 }
